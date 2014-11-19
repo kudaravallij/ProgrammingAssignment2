4c3ec09f0f262e6923420d5ec22aa8e66e89ff66
kudaravallij <jaya.kudaravalli6@gmail.com>
2014-11-19 15:30:18 -0600

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,74 @@
-## Put comments here that give an overall description of what your
-## functions do
+##################   FILE: cachematrix.R    ###############################
+#####  Author: Jaya Kudaravalli
+#####  Date:   11/19/2014
+#####  Description:
+#####  This file contains two functions makeCacheMatrix() and cacheSolve()
+#####  Please see function headers for description
+###########################################################################
 
-## Write a short comment describing this function
+
+
+
+##################  FUNCTION : makeCacheMatrix() ##########################
+## This function creates a list with the functions:
+## set(), get(), setinverse(), getinverse() 
+## For this function to work correctly the input data 
+## must form a inversible matrix
+## 
+## set() - set the value of matrix 
+## get() - get the value of matrix 
+## setinverse() -  set inverse matrix  
+## getinverse() -  get inverse matrix 
+###########################################################################
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+    im <- NULL
+    set <- function(y) {
+        ##message("Setting matrix")
+	    x <<- y
+	    im <<- NULL
+	}
 
+	get <- function() {
+	    ##message(" Getting matrix")
+		x
+	}
+
+    setinverse <- function(invmatrix) {
+ 	    ##message("Setting inverse matrix")
+	    im <<- invmatrix
+    }
+
+    getinverse <- function() {
+        ##message("Getting Inverse matrix")
+        im
+    }
+								  
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+##################  FUNCTION : cacheSolve() ##################################
+## This function creates inverse matrix for a special matrix created by
+## previous function makeCacheMatrix()
+## If an inverse matrix was never created, then it will compute inverse matrix.
+## If inverse matrix were created earlier and the original matrix did not change
+## then it returns "inverse matrix" computed earlier and stored in cache
+##############################################################################
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    im <- x$getinverse()
+    if(!is.null(im)) {
+        message("getting cached Matrix")
+        return(im)
+    }
+
+    data <- x$get()
+    im <- solve(data, ...)
+    x$setinverse(im)
+    im
 }
+
+
+
+
